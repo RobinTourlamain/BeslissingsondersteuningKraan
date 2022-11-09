0558d00df0bdeeed68ac48693707dc3c171861be
RobinTourlamain <robin.tourlamain@gmail.com>
2022-11-09 20:16:01 +0100

    Changes by NielsDesktop

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Container {
-    int index;
-    List<Coordinaat> coordinaten;
+    int id;
+    int length;
+    List<Slot> slots;
+
+    public Container(int id, int length) {
+        this.id = id;
+        this.length = length;
+        this.slots = new ArrayList<>();
+    }
+
+    public void assignSlot(Slot slot) {
+        this.slots.add(slot);
+        slot.containers.add(this);
+    }
 }
