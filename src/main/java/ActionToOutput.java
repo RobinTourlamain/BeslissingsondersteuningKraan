b35e0ca39d9030487d27ed20f338226c90078730
nielsaumatic <niels.de.walsche@gmail.com>
2022-12-21 22:24:13 +0100

    more cleanup

--- src/main/java/ActionToOutput.java
+++ src/main/java/ActionToOutput.java
@@ -24,36 +24,36 @@ public class ActionToOutput {
             index++;
         }
 
-        actionFrames.forEach(m -> {m.forEach((key, value)-> {
-            System.out.print("crane " + key + " container " + value.container.id + " from x " + value.prevSlot.x + " to x " + value.slot.x + ", ");
-        });
-            System.out.println();});
+//        actionFrames.forEach(m -> {m.forEach((key, value)-> {
+//            System.out.print("crane " + key + " container " + value.container.id + " from x " + value.prevSlot.x + " to x " + value.slot.x + ", ");
+//        });
+//            System.out.println();});
 
         return toOutputRecords(actionFrames, cranes);
     }
 
-    public static List<List<OutputRecord>> toOutputRecords(List<Map<Integer, Action>> actionlist, List<Crane> cranes){
+    public static List<List<OutputRecord>> toOutputRecords(List<Map<Integer, Action>> actionlist, List<Crane> cranes) {
         List<List<OutputRecord>> records = new ArrayList<>();
         List<Map<Integer, Action>> actions = new ArrayList<>(actionlist);
         double time = 0;
         int index = 0;
 
-        for(Map<Integer, Action> map : actions){
+        for (Map<Integer, Action> map : actions) {
             records.add(new ArrayList<>());
             double duration = 0;
-            for(Map.Entry<Integer, Action> entry : map.entrySet()){
+            for (Map.Entry<Integer, Action> entry : map.entrySet()) {
                 //move crane to pickup duration
-                double xdurationcrane = Math.abs(cranes.get(entry.getKey()).x - entry.getValue().prevSlot.x) / cranes.get(entry.getKey()).speedX;
-                double ydurationcrane = Math.abs(cranes.get(entry.getKey()).y - entry.getValue().prevSlot.y) / cranes.get(entry.getKey()).speedY;
-                double pickupduration = Math.max(xdurationcrane, ydurationcrane);
+                double xDurationCrane = Math.abs(cranes.get(entry.getKey()).x - entry.getValue().prevSlot.x) / cranes.get(entry.getKey()).speedX;
+                double yDurationCrane = Math.abs(cranes.get(entry.getKey()).y - entry.getValue().prevSlot.y) / cranes.get(entry.getKey()).speedY;
+                double pickupDuration = Math.max(xDurationCrane, yDurationCrane);
 
                 //get move duration
-                double xduration = Math.abs((double)entry.getValue().slot.x - entry.getValue().prevSlot.x) / cranes.get(entry.getKey()).speedX;
-                double yduration = Math.abs((double)entry.getValue().slot.y - entry.getValue().prevSlot.y) / cranes.get(entry.getKey()).speedY;
-                double moveduration = Math.max(xduration, yduration);
+                double xDuration = Math.abs((double)entry.getValue().slot.x - entry.getValue().prevSlot.x) / cranes.get(entry.getKey()).speedX;
+                double yDuration = Math.abs((double)entry.getValue().slot.y - entry.getValue().prevSlot.y) / cranes.get(entry.getKey()).speedY;
+                double moveDuration = Math.max(xDuration, yDuration);
 
-                if(pickupduration + moveduration > duration){
-                    duration = pickupduration + moveduration;
+                if (pickupDuration + moveDuration > duration) {
+                    duration = pickupDuration + moveDuration;
                 }
 
                 //add record
@@ -61,27 +61,27 @@ public class ActionToOutput {
                         new OutputRecord(
                                 entry.getKey(),
                                 entry.getValue().container.id,
-                                time + pickupduration,
-                                time + pickupduration + moveduration,
-                                entry.getValue().prevSlot.x + (double)(entry.getValue().container.length)/2,
+                                time + pickupDuration,
+                                time + pickupDuration + moveDuration,
+                                entry.getValue().prevSlot.x + (double) (entry.getValue().container.length / 2),
                                 entry.getValue().prevSlot.y + 0.5,
-                                entry.getValue().slot.x + (double)(entry.getValue().container.length)/2,
+                                entry.getValue().slot.x + (double) (entry.getValue().container.length / 2),
                                 entry.getValue().slot.y + 0.5,
                                 entry.getValue()
-                                )
+                        )
                 );
             }
 
             //move cranes without task out of way
-            for(Crane crane : cranes){
-                if(!map.containsKey(crane.id)){
+            for (Crane crane : cranes) {
+                if (!map.containsKey(crane.id)) {
                     List<Integer> safezone = getSafeSpaces(crane, map, cranes).stream().toList();
                     int safe;
-                    if(crane.id > cranes.size()/2){
+                    if (crane.id > cranes.size()/2) {
                         safe = safezone.get(0);
                     }
-                    else{
-                        safe = safezone.get(safezone.size()-1);
+                    else {
+                        safe = safezone.get(safezone.size() - 1);
                     }
                     records.get(index).add(
                             new OutputRecord(
@@ -105,16 +105,16 @@ public class ActionToOutput {
         return records;
     }
 
-    public  static Set<Integer> getSafeSpaces(Crane crane, Map<Integer, Action> actions, List<Crane> cranes){
+    public static Set<Integer> getSafeSpaces(Crane crane, Map<Integer, Action> actions, List<Crane> cranes) {
         Set<Integer> range = new HashSet<>();
-        for(int i = crane.xMin; i < crane.xMax; i++){
+        for (int i = crane.xMin; i < crane.xMax; i++) {
             range.add(i);
         }
         List<Crane> tasked = new ArrayList<>();
         actions.keySet().forEach(integer -> tasked.add(cranes.get(integer)));
-        for(Crane tc : tasked){
+        for (Crane tc : tasked) {
             Set<Integer> newrange = new HashSet<>();
-            for(int i = tc.xMin; i < tc.xMax; i++){
+            for (int i = tc.xMin; i < tc.xMax; i++) {
                 newrange.add(i);
             }
             range.removeAll(newrange);
@@ -134,21 +134,21 @@ public class ActionToOutput {
         slots.add(action.slot.x + action.container.length - 1);
         slots.add(action.prevSlot.x);
         slots.add(action.prevSlot.x + action.container.length - 1);
-        int leftmostslot = Collections.min(slots);
-        int rightmostslot = Collections.max(slots);
+        int leftMostSlot = Collections.min(slots);
+        int rightMostSlot = Collections.max(slots);
 
         for (Action a : consider) {
-            List<Integer> compareslots = new ArrayList<>();
-            compareslots.add(a.slot.x);
-            compareslots.add(a.slot.x + a.container.length - 1);
-            compareslots.add(a.prevSlot.x);
-            compareslots.add(a.prevSlot.x + a.container.length - 1);
-            int leftmostslotc = Collections.min(compareslots);
-            int rightmostslotc = Collections.max(compareslots);
+            List<Integer> compareSlots = new ArrayList<>();
+            compareSlots.add(a.slot.x);
+            compareSlots.add(a.slot.x + a.container.length - 1);
+            compareSlots.add(a.prevSlot.x);
+            compareSlots.add(a.prevSlot.x + a.container.length - 1);
+            int leftMostSlotCompare = Collections.min(compareSlots);
+            int rightMostSlotCompare = Collections.max(compareSlots);
 
-            if (leftmostslot <= rightmostslotc && rightmostslot >= leftmostslotc) {
-                System.out.println("overlap: " + leftmostslot + "," + rightmostslot + " binnen: " + leftmostslotc + "," + rightmostslotc + " lengte: " + a.container.length);
-                craneactions.forEach((key, value)-> System.out.println(key + " " + value.container.id  + " tussen " + value.slot.x + ", " + value.prevSlot.x ));
+            if (leftMostSlot <= rightMostSlotCompare && rightMostSlot >= leftMostSlotCompare) {
+                //System.out.println("overlap: " + leftMostSlot + "," + rightMostSlot + " binnen: " + leftMostSlotCompare + "," + rightMostSlotCompare + " lengte: " + a.container.length);
+                //craneactions.forEach((key, value)-> System.out.println(key + " " + value.container.id  + " tussen " + value.slot.x + ", " + value.prevSlot.x ));
                 return true;
             }
         }
@@ -162,20 +162,20 @@ public class ActionToOutput {
         slots.add(action.prevSlot.x);
         slots.add(action.prevSlot.x + action.container.length - 1);
 
-        int leftmostslot = Collections.min(slots);
-        int rightmostslot = Collections.max(slots);
+        int leftMostSlot = Collections.min(slots);
+        int rightMostSlot = Collections.max(slots);
 
         if (action.container.length > 1) {
-            leftmostslot++;
-            rightmostslot--;
+            leftMostSlot++;
+            rightMostSlot--;
         }
 
-        if (crane.xMin <= leftmostslot && rightmostslot <= crane.xMax) {
-            System.out.println("kan executen: " + action.container.id + " naar x " + action.container.slots.get(0).x);
+        if (crane.xMin <= leftMostSlot && rightMostSlot <= crane.xMax) {
+            //System.out.println("kan executen: " + action.container.id + " naar x " + action.container.slots.get(0).x);
             return true;
         }
-        System.out.println("kan niet executen");
-        System.out.println(leftmostslot + "," + rightmostslot + " niet binnen: " + crane.xMin + "," + crane.xMax + " length: " + action.container.length);
+        //System.out.println("kan niet executen");
+        //System.out.println(leftMostSlot + "," + rightMostSlot + " niet binnen: " + crane.xMin + "," + crane.xMax + " length: " + action.container.length);
         return false;
     }
 
